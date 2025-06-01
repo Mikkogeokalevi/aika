fead3e1b6bf7db11f1ff61b9efecd06c90b15880
Mikkogeokalevi <mikkogeokalevi@gmail.com>
2025-06-01 21:27:31 +0300

    Add files via upload

---
+++ puzzle_data.js
@@ -0,0 +1,7 @@
+const puzzleSetup = {
+    wrongInputMessage: "Väärin meni!",
+    steps: [
+        { expectedInput: () => "testi", responseMessage: "Oikein!" },
+        { expectedInput: () => "toinen", responseMessage: "Hyvin menee!" }
+    ]
+};
